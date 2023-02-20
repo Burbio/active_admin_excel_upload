b0c62d7cbb2fa5fd2c57af1ab2c9391a76157673
Brian Cicutti <brian@cicutti.com>
2023-02-20 03:49:14 -0500

    fixes for rails 7

--- lib/active_admin_excel_upload/excel_parsable.rb
+++ lib/active_admin_excel_upload/excel_parsable.rb
@@ -5,7 +5,7 @@ module ActiveAdminExcelUpload
     class_methods do
       def publish_to_channel(channel_name,message)
         message = "[#{self.to_s}]#{message}"
-        ActionCable.server.broadcast channel_name, message: message
+        ActionCable.server.broadcast channel_name, message
       end
 
       def excel_create_record(row, index, header,channel_name)
@@ -15,8 +15,15 @@ module ActiveAdminExcelUpload
         if record.save
           self.publish_to_channel(channel_name,"Successfully created record for #{row}, id: #{record.id}")
         else
-          self.publish_to_channel(channel_name,"Could not create record for #{row}, error: #{record.errors.messages}")
+          # self.publish_to_channel(channel_name,"Could not create record for #{row}, error: #{record.errors.messages}")
+          self.publish_to_channel(channel_name,more_delightful_message(row,record.errors.messages))
         end
+      end
+
+      def more_delightful_message(row, messages)
+        first_message = row.kind_of?(String) ? row : row.try(:[],0)
+        error_messages = messages[:error].map{|k,v| "<p>#{k.to_s} #{v.to_s}</p>"}.join("").html_safe
+        "<p>Could not create record for row #{first_message}.</p>".html_safe + error_messages
       end
 
 
@@ -25,7 +32,9 @@ module ActiveAdminExcelUpload
         sheet = xlsx.sheet(xlsx.sheets.index(self.table_name))
         header = sheet.row(1)
         channel_name = "excel_channel_#{current_admin_user.id}"
+        logger.info "Brian :: #{channel_name}"
         header_downcase = header.map(&:parameterize).map(&:underscore)
+        log_table_name(self)
         self.publish_to_channel(channel_name,"Start processing sheet #{self.table_name}")
         sheet.parse.each_with_index do |row, index|
           begin
@@ -35,6 +44,16 @@ module ActiveAdminExcelUpload
           end
         end
         self.publish_to_channel(channel_name, "End processing sheet #{self.table_name}")
+      end
+
+      def log_table_name(itself)
+        begin
+          logger.info "#{itself.table_name}"
+        rescue => e 
+          logger.info "ANNABELLE -- JOB FAILED"
+          logger.info itself 
+          logger.info e.message
+        end
       end
     end
   end
